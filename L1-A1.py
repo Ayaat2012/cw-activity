5d7cd934130274c4b790a9ddad9e939155d39eb4
Ayaat2012 <iffatrajshahi@gmail.com>
2025-07-12 19:41:53 +0600

    initial commit

---
+++ L1-A1.py
@@ -0,0 +1,16 @@
+# Printing a greeting message
+print("Hi!")
+
+# Printing my name
+print("My name is Ayaat.")
+
+# Printing a fun fact about myself
+print("I love drawing, specifically sketching with pens and pencils.")
+
+# Performing and printing a simple calculation
+print("The dum of 4 and 3 is:-", 4 + 3)
+
+'''
+This is
+Multiple line comments
+'''

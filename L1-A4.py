6db1b40fc4aabe3e3603e8586818513a3fd22069
Ayaat2012 <iffatrajshahi@gmail.com>
2025-07-12 19:55:38 +0600

    initial commit

---
+++ L1-A4.py
@@ -0,0 +1,15 @@
+# Printing a sentence
+print("Welcome to Python!")
+
+# Printing a number
+print(7)
+
+# Next line after a print
+print("Hello Python \n This sentence is coming in a new line..")
+
+# Another example
+print("Hi! \n \n \n There's a gap of 3 lines now.")
+
+# Print all the keywords present in Python. Print("Keywords in Python are:...")
+import keyword 
+print(keyword.kwlist)

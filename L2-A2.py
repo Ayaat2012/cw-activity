b53b075ba032bfb4a9d880d78a79eec3e3fe5a43
Ayaat2012 <iffatrajshahi@gmail.com>
2025-07-18 19:53:08 +0600

    initial commit

---
+++ L2-A2.py
@@ -0,0 +1,21 @@
+num1 = 45
+num2 = 3
+
+print("Number 1 is", num1)
+print("Number 2 is", num2)
+print("Addition :", num1+num2)
+print("Difference :", num1-num2)
+print("Product :", num1*num2)
+print("Divition :", num1/num2) #One DIV sign gives results in float datatype
+print("Floor Divition :", num1//num2) #Two DIV signs give results in integer datatype
+print("Modulus Operator :", num1%num2)
+print("Square :", num2**2)
+print("Squere Root :", num1**0.5)
+
+print("Equal ?", num1==num2)
+print("Is Number 1 greater ?", num1>num2)
+print("Is Number 2 greater ?", num1<num2)
+print("Not Equal ?", num1!=num2)
+
+result = num1/2+num2**2+10
+print("The result of the equation is :", result)

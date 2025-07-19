6b1677599449a0c8f0893820819aa82d9bc4ba44
Ayaat2012 <iffatrajshahi@gmail.com>
2025-07-19 19:35:40 +0600

    initial commit

---
+++ L3-A2.py
@@ -0,0 +1,23 @@
+height = float(input("Enter your height in cm :"))
+weight = float(input("Enter your weight in kg :"))
+
+BMI = weight / (height/100)**2
+print("Your BMI is ", BMI)
+
+if BMI <= 18.4:
+    print("You're under-weight")
+
+elif BMI <= 24.9:
+    print("You're healthy")
+
+elif BMI <= 29.9:
+    print("You're over-weight")
+
+elif BMI <= 34.9:
+    print("You're severely over-weight")
+
+elif BMI <= 39.9:
+    print("You're obese")
+
+else:
+    print("You're severely obese")

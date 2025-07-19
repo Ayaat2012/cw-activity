14f0e18ff917eab38fa37852a44c67109205cdda
Ayaat2012 <iffatrajshahi@gmail.com>
2025-07-19 19:56:50 +0600

    initial commit

---
+++ L3-A4.py
@@ -0,0 +1,10 @@
+import datetime, calendar
+
+#Using 'now()' to get current time
+current_time = datetime.datetime.today()
+
+#Printing value of now
+print("The current time at greenwich meridian is :", current_time)
+
+#Print calender of year 2025
+print("\n", calendar.calendar(2025))
